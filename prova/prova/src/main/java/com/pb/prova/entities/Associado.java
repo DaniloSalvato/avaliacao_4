2a35a06b0adfc3e5ae2005fb96b6ea69dbc0ef6d
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:47:26 -0300

    criando osatributos do associado, o contrutor e o converter

--- prova/prova/src/main/java/com/pb/prova/entities/Associado.java
+++ prova/prova/src/main/java/com/pb/prova/entities/Associado.java
@@ -1,12 +1,19 @@
 package com.pb.prova.entities;
 
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+
 import com.pb.prova.constants.Cargo;
 import com.pb.prova.constants.Sexo;
+
+import com.pb.prova.serializer.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -19,9 +26,17 @@ public class Associado{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String dataDeNascimento;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate dataDeNascimento;
     @Enumerated(EnumType.STRING)
     private Cargo cargo;
     @Enumerated(EnumType.STRING)
     private Sexo sexo;
+
+    public Associado(String nome, LocalDate dataDeNascimento, Cargo cargo, Sexo sexo) {
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+        this.cargo = cargo;
+        this.sexo = sexo;
+    }
 }
