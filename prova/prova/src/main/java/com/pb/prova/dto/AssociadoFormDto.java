9959df7999fe1776f684b5a8f2004fdb307aae4e
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:43:44 -0300

    adicionando um converter e criando um  construtor atualizar

--- prova/prova/src/main/java/com/pb/prova/dto/AssociadoFormDto.java
+++ prova/prova/src/main/java/com/pb/prova/dto/AssociadoFormDto.java
@@ -2,22 +2,45 @@ package com.pb.prova.dto;
 
 import com.pb.prova.constants.Cargo;
 import com.pb.prova.constants.Sexo;
-
+import com.pb.prova.entities.Associado;
+import com.pb.prova.repository.AssociadoRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import java.util.Date;
+import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssociadoFormDto {
 
     @NotNull(message = "O campo nome não pode ser vazio!")
     private String nome;
 
     @NotNull(message = "O campo sigla não pode ser vazio!")
-    private Date dataDeNascimento;
+    private LocalDate dataDeNascimento;
 
     @NotNull(message = "O campo ideologia não pode ser vazio, e precisa pertencer a Direita, Esquerda ou Centro!")
     private Cargo cargo;
 
     @NotNull(message = "O campo data de fundação não pode ser vazio!")
     private Sexo sexo;
+
+
+
+    public Associado atualizar(Long id, AssociadoRepository associadoRepository) {
+        Associado associado = associadoRepository.getById(id);
+
+        associado.setNome(nome);
+        associado.setDataDeNascimento(dataDeNascimento);
+        associado.setCargo(cargo);
+        associado.setSexo(sexo);
+        return associado;
+    }
+
+    public Associado converter(AssociadoRepository associadoRepository){
+        return new Associado(this.nome, this.dataDeNascimento, this.cargo, this.sexo);
+    }
 }
