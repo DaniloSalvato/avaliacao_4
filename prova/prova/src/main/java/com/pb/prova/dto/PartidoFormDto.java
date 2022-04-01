2195a2bba3ff232184cf4f82115e878c5e6d0474
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:45:58 -0300

    criando construtor e converter

--- prova/prova/src/main/java/com/pb/prova/dto/PartidoFormDto.java
+++ prova/prova/src/main/java/com/pb/prova/dto/PartidoFormDto.java
@@ -2,17 +2,21 @@ package com.pb.prova.dto;
 
 import com.pb.prova.constants.Ideologia;
 
+import com.pb.prova.entities.Partido;
+import com.pb.prova.repository.PartidoRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PartidoFormDto {
+
 
     @NotNull(message = "O campo nome não pode ser vazio!")
     private String nomeDoPartido;
@@ -24,7 +28,20 @@ public class PartidoFormDto {
     private Ideologia ideologia;
 
     @NotNull(message = "O campo data de fundação não pode ser vazio!")
-    private Date dataDeFuncadacao;
+    private LocalDate dataDeFundacao;
 
 
+    public Partido converter(PartidoRepository partidoRepository){
+        return new Partido(this.nomeDoPartido, this.sigla, this.ideologia, this.dataDeFundacao);
+    }
+
+    public Partido atualizar(Long id, PartidoRepository partidoRepository){
+        Partido partido = partidoRepository.getOne(id);
+
+        partido.setNomeDoPartido(nomeDoPartido);
+        partido.setSigla(sigla);
+        partido.setIdeologia(ideologia);
+        partido.setDataDeFundacao(dataDeFundacao);
+        return partido;
+    }
 }
