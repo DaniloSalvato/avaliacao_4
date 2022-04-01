3066f877b1b5eca33cd16bbaf4e38431b75d88f2
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:50:50 -0300

    arrumando a paginação e a lista

--- prova/prova/src/main/java/com/pb/prova/service/PartidoService.java
+++ prova/prova/src/main/java/com/pb/prova/service/PartidoService.java
@@ -2,13 +2,19 @@ package com.pb.prova.service;
 
 import com.pb.prova.constants.Ideologia;
 import com.pb.prova.dto.PartidoDto;
+import com.pb.prova.dto.PartidoFormDto;
 import com.pb.prova.entities.Partido;
 import com.pb.prova.repository.PartidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,16 +24,14 @@ public class PartidoService {
     @Autowired
     PartidoRepository partidoRepository;
 
-    public List<PartidoDto> listarTodos(Ideologia ideologia, String ordenacao) {
-
-        Sort sort = Sort.by("id").ascending();
+    public Page<PartidoDto> listarTodos(Ideologia ideologia, Pageable ordenacao) {
 
         if (ideologia == null) {
-            List<Partido> partidos = partidoRepository.findAll(sort);
-            return PartidoDto.converter(partidos);
+            List<Partido> partidos = partidoRepository.findAll();
+            return PartidoDto.converter(new PageImpl<>(partidos));
         } else {
-            List<Partido> partidos = partidoRepository.findByIdeologia(ideologia, sort);
-            return PartidoDto.converter(partidos);
+            List<Partido> partidos = partidoRepository.findByIdeologia(ideologia, ordenacao);
+            return PartidoDto.converter(new PageImpl<>(partidos));
         }
     }
 
@@ -38,4 +42,30 @@ public class PartidoService {
         }
         return ResponseEntity.notFound().build();
     }
+
+    public ResponseEntity<PartidoDto> cadastrar(PartidoFormDto partidoFormDto, UriComponentsBuilder uriBuilder) {
+        Partido partido = partidoFormDto.converter(partidoRepository);
+        partidoRepository.save(partido);
+        URI uri = uriBuilder.path("/partidos").buildAndExpand(partido.getId()).toUri();
+        return ResponseEntity.created(uri).body(new PartidoDto(partido));
+    }
+
+    public ResponseEntity<PartidoDto> atualizarPorId(Long id, PartidoFormDto partidoFormDto) {
+        Optional<Partido> optional = partidoRepository.findById(id);
+        if(optional.isPresent()){
+            Partido partido = partidoFormDto.atualizar(id, partidoRepository);
+            return ResponseEntity.ok(new PartidoDto(partido));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<Object> excluirPorId(Long id) {
+        Optional<Partido> optional = partidoRepository.findById(id);
+        if(optional.isPresent()){
+            partidoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
