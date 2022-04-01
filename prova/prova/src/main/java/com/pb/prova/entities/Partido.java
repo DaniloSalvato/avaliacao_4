b75c9c988f6a9e00d885eb4c6f0693cdffdbccee
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:58:53 -0300

    implementando a entidade

--- prova/prova/src/main/java/com/pb/prova/entities/Partido.java
+++ prova/prova/src/main/java/com/pb/prova/entities/Partido.java
@@ -1,12 +1,17 @@
 package com.pb.prova.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import com.pb.prova.constants.Ideologia;
+
+import com.pb.prova.serializer.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Data
 @AllArgsConstructor
@@ -22,6 +27,13 @@ public class Partido {
     private String sigla;
     @Enumerated(EnumType.STRING)
     private Ideologia ideologia;
-    private Date dataDeFundacao;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate dataDeFundacao;
 
+    public Partido(String nomeDoPartido, String sigla, Ideologia ideologia, LocalDate dataDeFundacao) {
+        this.nomeDoPartido = nomeDoPartido;
+        this.sigla = sigla;
+        this.ideologia = ideologia;
+        this.dataDeFundacao = dataDeFundacao;
+    }
 }
