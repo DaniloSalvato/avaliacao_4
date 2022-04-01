a9f2757e23e3544eeb32cfb605b9f01a30fc9806
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:50:00 -0300

    arrumando a paginação

--- prova/prova/src/main/java/com/pb/prova/repository/PartidoRepository.java
+++ prova/prova/src/main/java/com/pb/prova/repository/PartidoRepository.java
@@ -2,13 +2,14 @@ package com.pb.prova.repository;
 
 import com.pb.prova.constants.Ideologia;
 import com.pb.prova.entities.Partido;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PartidoRepository extends JpaRepository<Partido, Long> {
-    List<Partido> findByIdeologia(Ideologia ideologia, Sort sort);
 
-    List<Partido> findAll(Sort sort);
+    List<Partido> findByIdeologia(Ideologia ideologia, Pageable ordenacao);
 }
