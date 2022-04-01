bd996caa5dd846b5a5ed20295d660b8df2723029
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:53:50 -0300

    Arrumando a paginação e a lista

---
+++ prova/prova/src/main/java/com/pb/prova/repository/AssociadoRepository.java
@@ -0,0 +1,15 @@
+package com.pb.prova.repository;
+
+import com.pb.prova.constants.Cargo;
+import com.pb.prova.entities.Associado;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AssociadoRepository extends JpaRepository<Associado, Long> {
+
+    List<Associado> findByCargo(Cargo cargo, Pageable ordenacao);
+}
