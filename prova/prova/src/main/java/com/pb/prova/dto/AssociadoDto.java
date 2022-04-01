2d8649e54b1da2d20941d22e8449065484378d16
Danilo <danilo_salvato@hotmail.com>
2022-04-01 19:41:18 -0300

    adicionando construtor e o converter

--- prova/prova/src/main/java/com/pb/prova/dto/AssociadoDto.java
+++ prova/prova/src/main/java/com/pb/prova/dto/AssociadoDto.java
@@ -1,16 +1,39 @@
 package com.pb.prova.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.pb.prova.constants.Cargo;
 import com.pb.prova.constants.Sexo;
+import com.pb.prova.entities.Associado;
+import com.pb.prova.serializer.LocalDateSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssociadoDto {
 
     private Long id;
     private String nome;
-    private String dataDeNascimento;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate dataDeNascimento;
     private Cargo cargo;
     private Sexo sexo;
+
+    public AssociadoDto(Associado associado){
+        this.id = associado.getId();
+        this.nome = associado.getNome();
+        this.dataDeNascimento = associado.getDataDeNascimento();
+        this.cargo = associado.getCargo();
+        this.sexo = associado.getSexo();
+    }
+
+    public static Page<AssociadoDto> converter(Page<Associado> associados) {
+        return associados.map(AssociadoDto::new);
+    }
 }
