f067c065fa2fa1eedf990e287f99edac9cbd7492
Danilo <danilo_salvato@hotmail.com>
2022-04-03 14:13:50 -0300

    melhorando a pesquisa de url

--- prova/prova/src/main/java/com/pb/prova/config/UpperCaseSexo.java
+++ prova/prova/src/main/java/com/pb/prova/config/UpperCaseSexo.java
@@ -1,2 +1,12 @@
-package com.pb.prova.config;public class UpperCaseSexo {
+package com.pb.prova.config;
+
+import com.pb.prova.constants.Sexo;
+import org.springframework.core.convert.converter.Converter;
+
+public class UpperCaseSexo implements Converter<String,Sexo> {
+
+    @Override
+    public Sexo convert(String value){
+        return Sexo.valueOf(value.toUpperCase());
+    }
 }
