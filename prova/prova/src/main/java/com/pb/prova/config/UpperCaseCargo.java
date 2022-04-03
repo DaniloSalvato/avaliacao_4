3072009f23f482ab176fb1e7d2d8e100bb62d4d4
Danilo <danilo_salvato@hotmail.com>
2022-04-03 14:14:50 -0300

    melhorando a pesquisa de url

--- prova/prova/src/main/java/com/pb/prova/config/UpperCaseCargo.java
+++ prova/prova/src/main/java/com/pb/prova/config/UpperCaseCargo.java
@@ -1,2 +1,13 @@
-package com.pb.prova.config;public class UpperCaseCargo {
+package com.pb.prova.config;
+
+import com.pb.prova.constants.Cargo;
+import org.springframework.core.convert.converter.Converter;
+
+public class UpperCaseCargo  implements Converter<String, Cargo> {
+
+    @Override
+    public Cargo convert(String value){
+        return Cargo.valueOf(value.toUpperCase());
+    }
+
 }
