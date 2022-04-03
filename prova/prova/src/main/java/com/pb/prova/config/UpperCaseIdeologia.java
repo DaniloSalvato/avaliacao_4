2866e10717ffb4961af1131affc2e631f8510c4c
Danilo <danilo_salvato@hotmail.com>
2022-04-03 14:14:25 -0300

    melhorando a pesquisa de url

--- prova/prova/src/main/java/com/pb/prova/config/UpperCaseIdeologia.java
+++ prova/prova/src/main/java/com/pb/prova/config/UpperCaseIdeologia.java
@@ -1,2 +1,13 @@
-package com.pb.prova.config;public class UpperCaseIdeologia {
+package com.pb.prova.config;
+
+
+import com.pb.prova.constants.Ideologia;
+import org.springframework.core.convert.converter.Converter;
+
+public class UpperCaseIdeologia implements Converter<String, Ideologia> {
+
+    @Override
+    public Ideologia convert(String value){
+        return Ideologia.valueOf(value.toUpperCase());
+    }
 }
